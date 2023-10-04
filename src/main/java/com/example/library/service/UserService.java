8b85536510e6b7218739a1ca86329afef48290d7
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-10-04 14:46:08 +0300

    Added classes for registration

---
+++ src/main/java/com/example/library/service/UserService.java
@@ -0,0 +1,56 @@
+package com.example.library.service;
+
+import com.example.library.entity.user.Role;
+import com.example.library.entity.user.User;
+import com.example.library.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    public User save(User user){
+        if (userRepository.findByUsername(user.getUsername()).isPresent()) {
+            throw new RuntimeException(String.format("User with username - %s already existed", user.getUsername()));
+        }
+        user.setRoles(Set.of(Role.ADMIN));
+        user.setPassword(passwordEncoder().encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    @Transactional(readOnly = true)
+    public User findByUsername(String username){
+        return checkUser(userRepository.findByUsername(username));
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUsername(username);
+        if (user.isEmpty()) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return user.get();
+    }
+
+    private User checkUser(Optional<User> user){
+        if (user.isEmpty()) {
+            throw new RuntimeException(String.format("User @%s not found", user.get().getUsername()));
+        }
+        return user.get();
+    }
+}
