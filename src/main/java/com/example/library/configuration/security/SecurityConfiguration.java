5a02849e8583fa6d2a456c85edb8ee6f04047824
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-10-04 14:44:34 +0300

    Added security configuration and jwt classes

---
+++ src/main/java/com/example/library/configuration/security/SecurityConfiguration.java
@@ -0,0 +1,43 @@
+package com.example.library.configuration.security;
+
+import com.example.library.configuration.security.jwt.JWTTokenFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private final JWTTokenFilter jwtTokenFilter;
+
+    private static final String LOGIN_ENDPOINT = "/api/user/auth/**";
+    private static final String REG_ENDPOINT = "/api/user";
+    private static final String ADMIN_ENDPOINT = "/**/admin/**";
+    private static final String ROLE_ADMIN = "ADMIN";
+    private static final String SWAGGER_ENDPOINT = "/swagger-ui/**";
+    private static final String OPEN_API_ENDPOINT = "/javainuse-openapi/**";
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(LOGIN_ENDPOINT).permitAll()
+                .antMatchers(REG_ENDPOINT).permitAll()
+                .antMatchers(ADMIN_ENDPOINT).hasAuthority(ROLE_ADMIN)
+                .antMatchers(SWAGGER_ENDPOINT, OPEN_API_ENDPOINT).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+        http
+                .headers().frameOptions().sameOrigin();
+    }
+}
