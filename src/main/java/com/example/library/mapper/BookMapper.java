d24abdf7b6982f1e8ee60de8443e569c45a97851
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-10-06 16:09:26 +0300

    Added BookMapper

---
+++ src/main/java/com/example/library/mapper/BookMapper.java
@@ -0,0 +1,16 @@
+package com.example.library.mapper;
+
+import com.example.library.dto.BookDto;
+import com.example.library.dto.UpdatedBookDto;
+import com.example.library.entity.library.book.Book;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface BookMapper {
+    BookMapper INSTANCE = Mappers.getMapper(BookMapper.class);
+
+    Book dtoToBook(BookDto dto);
+    BookDto bookToDto(Book book);
+    Book updatedDtoToUser(UpdatedBookDto dto);
+}
