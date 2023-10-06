2450ff5c7875a60509af3947b4a24748b4e21d66
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-10-06 14:30:54 +0300

    Added new annotation - @Getter

--- src/main/java/com/example/library/entity/AbstractEntity.java
+++ src/main/java/com/example/library/entity/AbstractEntity.java
@@ -1,11 +1,14 @@
 package com.example.library.entity;
 
+import lombok.Getter;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
+@Getter
 public abstract class AbstractEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
