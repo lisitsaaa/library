8e07eefb8c259582a5a58b255c65ce37038e790a
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-10-06 14:31:18 +0300

    Removed a field books

--- src/main/java/com/example/library/entity/library/book/Author.java
+++ src/main/java/com/example/library/entity/library/book/Author.java
@@ -3,11 +3,8 @@ package com.example.library.entity.library.book;
 import com.example.library.entity.AbstractEntity;
 import lombok.*;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import java.util.List;
 
 @Entity
 @Table
@@ -17,7 +14,4 @@ public class Author extends AbstractEntity {
     private String name;
     private String surname;
     private String parentName;
-
-    @ManyToMany(mappedBy = "authors", cascade = CascadeType.ALL)
-    private List<Book> books;
 }
