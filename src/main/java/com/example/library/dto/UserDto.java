59d03ba662c64a819ef523794f58ee5ab76953a4
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-10-09 12:33:24 +0300

    Add a class - UserDto

--- src/main/java/com/example/library/dto/UserDto.java
+++ src/main/java/com/example/library/dto/UserDto.java
@@ -1,19 +1,14 @@
 package com.example.library.dto;
 
-import com.example.library.entity.user.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Set;
-
 @Data
-@AllArgsConstructor @NoArgsConstructor
+@NoArgsConstructor @AllArgsConstructor
 public class UserDto {
     private String name;
     private String surname;
     private String parentName;
     private String username;
-    private String password;
-    private Set<Role> roles;
 }
