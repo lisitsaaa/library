5d53415d01591777002ed7cecc2e92755df17c2c
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-10-12 00:14:22 +0300

    Added a class TokenDto

---
+++ src/main/java/com/example/library/dto/user/TokenDto.java
@@ -0,0 +1,11 @@
+package com.example.library.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor @AllArgsConstructor
+public class TokenDto {
+    private String token;
+}
