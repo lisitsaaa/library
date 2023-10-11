4154334058008d9558da2ebab9abb9bbcebabbe1
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-10-12 00:21:27 +0300

    Deleted a field reader

--- src/main/java/com/example/library/entity/library/Journal.java
+++ src/main/java/com/example/library/entity/library/Journal.java
@@ -2,13 +2,10 @@ package com.example.library.entity.library;
 
 import com.example.library.entity.AbstractEntity;
 import com.example.library.entity.library.book.Book;
-import com.example.library.entity.user.User;
 import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -18,9 +15,6 @@ import java.util.List;
 public class Journal extends AbstractEntity {
     private LocalDate dateOfGetBook;
     private LocalDate dateOfReturnBook;
-
-    @OneToOne
-    private User reader;
 
     @OneToMany
     private List<Book> book;
