e8600c01cc8616b6ff9056691fc2cace7b6dc8df
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-10-12 00:13:14 +0300

    Updated UserDto

--- src/main/java/com/example/library/dto/user/UserDto.java
+++ src/main/java/com/example/library/dto/user/UserDto.java
@@ -1,15 +1,18 @@
-package com.example.library.dto;
+package com.example.library.dto.user;
 
+import com.example.library.entity.user.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import java.util.Set;
 
 @Data
-@NoArgsConstructor @AllArgsConstructor
+@AllArgsConstructor @NoArgsConstructor
 public class UserDto {
     @NotNull @NotBlank @NotEmpty
     private String name;
@@ -20,5 +23,12 @@ public class UserDto {
     @NotNull @NotBlank @NotEmpty
     private String parentName;
 
+    @NotNull @NotBlank @NotEmpty
     private String username;
+
+    @NotNull @NotBlank @NotEmpty
+    @Length(min = 2, max = 16)
+    private String password;
+
+    private Set<Role> roles;
 }
