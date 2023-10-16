10f31fc3ed7add8bf8aa7e281717fafb42361be6
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-10-16 10:46:02 +0300

    Updated controller classes - add annotations for swagger

--- src/main/java/com/example/library/controller/BookController.java
+++ src/main/java/com/example/library/controller/BookController.java
@@ -5,6 +5,8 @@ import com.example.library.dto.book.BookListDto;
 import com.example.library.dto.book.UpdatedBookDto;
 import com.example.library.entity.library.book.BookStatus;
 import com.example.library.wrapper.BookWrapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.validation.BindingResult;
@@ -15,31 +17,37 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/api/book")
 @RequiredArgsConstructor
+@Tag(name = "Book", description = "Operations with books")
 public class BookController {
     private final BookWrapper bookWrapper;
 
     @PostMapping()
+    @Operation(summary = "Create a book")
     public BookDto create(@RequestBody @Valid BookDto bookDto,
                                           BindingResult bindingResult){
         return bookWrapper.save(bookDto, bindingResult);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get a book by ID")
     public BookDto getById(@PathVariable long id){
         return bookWrapper.getById(id);
     }
 
     @GetMapping("/isbn/{isbn}")
+    @Operation(summary = "Get a book by ISBN (International Standard Book Number)")
     public BookDto getByISBN(@PathVariable String isbn){
         return bookWrapper.getByIsbn(isbn);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Remove a book")
     public void remove(@PathVariable long id){
         bookWrapper.remove(id);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update information about book (ISBN, title, description, authors or genres)")
     public BookDto updateInfo(@RequestBody @Valid UpdatedBookDto bookDto,
                               BindingResult bindingResult,
                               @PathVariable long id){
@@ -47,11 +55,13 @@ public class BookController {
     }
 
     @GetMapping("/inside")
+    @Operation(summary = "Get books, which book status - INSIDE")
     public BookListDto getInsideBook(){
         return bookWrapper.getAllInside();
     }
 
     @GetMapping()
+    @Operation(summary = "Get all books with pagination")
     public BookListDto getAllWithPagination(
             @RequestParam(required = false, defaultValue = "0") int page,
             @RequestParam(required = false, defaultValue = "5") int size){
@@ -59,6 +69,7 @@ public class BookController {
     }
 
     @PutMapping("/status/{bookStatus}/{id}")
+    @Operation(summary = "Update book status")
     public BookDto updateBookStatus(@PathVariable BookStatus bookStatus,
                                     @PathVariable long id){
         return bookWrapper.updateBookStatus(bookStatus, id);
--- src/main/java/com/example/library/controller/UserController.java
+++ src/main/java/com/example/library/controller/UserController.java
@@ -4,6 +4,8 @@ import com.example.library.dto.user.AuthDto;
 import com.example.library.dto.user.TokenDto;
 import com.example.library.dto.user.UserDto;
 import com.example.library.wrapper.UserWrapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,16 +18,19 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
+@Tag(name = "User", description = "Operations with users")
 public class UserController {
     private final UserWrapper userWrapper;
 
     @PostMapping()
+    @Operation(summary = "Registration")
     public UserDto registration(@RequestBody @Valid UserDto userDto,
                                 BindingResult bindingResult) {
         return userWrapper.save(userDto, bindingResult);
     }
 
     @PostMapping("/auth")
+    @Operation(summary = "Authorization")
     public TokenDto authorization(@RequestBody @Valid AuthDto authDto,
                                   BindingResult bindingResult) {
         return userWrapper.login(authDto, bindingResult);
